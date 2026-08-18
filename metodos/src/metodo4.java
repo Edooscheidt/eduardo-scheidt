ede7f855ff39949c29f00688c5d7cde631f4685d
Edoo Scheidt <edooardoscheidt@gmail.com>
2026-08-18 18:49:51 -0300

    adiciona exercicio

--- metodos/src/metodo4.java
+++ metodos/src/metodo4.java
@@ -1,2 +1,62 @@
+import java.util.Scanner;
+
 public class metodo4 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        String inscricaoMaisAlto = "";
+        double alturaMaisAlto = 0;
+
+        String inscricaoMaisBaixo = "";
+        double alturaMaisBaixo = Double.MAX_VALUE;
+
+        double somaAlturas = 0;
+        int totalAtletas = 0;
+
+        String inscricao = "";
+
+        while (!inscricao.equals("0")) {
+            System.out.print("Digite o número de inscrição (ou 0 para sair): ");
+            inscricao = leitor.nextLine();
+
+            if (!inscricao.equals("0")) {
+                System.out.print("Digite a altura do atleta: ");
+                double altura = leitor.nextDouble();
+                leitor.nextLine();
+
+                totalAtletas++;
+                somaAlturas += altura;
+
+                if (altura > alturaMaisAlto) {
+                    alturaMaisAlto = altura;
+                    inscricaoMaisAlto = inscricao;
+                }
+
+                if (altura < alturaMaisBaixo) {
+                    alturaMaisBaixo = altura;
+                    inscricaoMaisBaixo = inscricao;
+                }
+            }
+        }
+
+        if (totalAtletas > 0) {
+            double mediaAlturas = somaAlturas / totalAtletas;
+
+            System.out.println("\nAtleta mais alto - Inscrição : " + inscricaoMaisAlto
+                    + " | Altura : " + alturaMaisAlto + "m");
+
+            System.out.println("Atleta mais baixo - Inscrição : " + inscricaoMaisBaixo
+                    + " | Altura : " + alturaMaisBaixo + "m");
+
+            System.out.println("Altura média do grupo   : "
+                    + String.format("%.2f", mediaAlturas) + "m");
+
+            System.out.println("Quantidade de atletas cadastrados: "
+                    + totalAtletas);
+        } else {
+            System.out.println("\nNenhum atleta foi cadastrado.");
+        }
+
+        leitor.close();
+    }
 }
---
+++ metodos/src/metodo5.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class metodo5 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int[] numero = new int[5];
+
+        for (int i = 0; i < numero.length; i++) {
+            System.out.print("Informe o " + (i + 1) + "º número : ");
+            numero[i] = leitor.nextInt();
+        }
+
+        leitor.close();
+
+        int maior = numero[0];
+        int menor = numero[0];
+        double soma = 0;
+
+        for (int i = 0; i < numero.length; i++) {
+            if (numero[i] > maior) {
+                maior = numero[i];
+            }
+
+            if (numero[i] < menor) {
+                menor = numero[i];
+            }
+
+            soma += numero[i];
+        }
+
+        double media = soma / numero.length;
+
+        System.out.println("Maior elemento : " + maior);
+        System.out.println("Menor elemento : " + menor);
+        System.out.println("Média dos elementos : " + media);
+
+        leitor.close();
+
+    }
+}
