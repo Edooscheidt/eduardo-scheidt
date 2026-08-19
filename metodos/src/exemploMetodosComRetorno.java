dcd83a171d046093cc2d2c6609a0fd9e000e063b
Edoo Scheidt <edooardoscheidt@gmail.com>
2026-08-19 20:45:02 -0300

    .

---
+++ metodos/src/exemploMetodosComRetorno.java
@@ -0,0 +1,18 @@
+public class exemploMetodosComRetorno {
+
+    public static double somarNumeros(int num1, int num2) {
+
+        //  double soma = num1 + num2;
+        // return soma;
+        return num1 + num2;
+    }
+
+   
+    public static void main(String[] args) {
+
+        double soma = somarNumeros(15, 50);
+        System.out.println(soma);
+
+        System.out.println(somarNumeros(10, 6));
+    }
+}
