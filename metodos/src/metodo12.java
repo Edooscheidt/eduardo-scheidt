f736cace859461a6b2f6cf3a0061b780c16da636
Edoo Scheidt <edooardoscheidt@gmail.com>
2026-08-26 19:21:50 -0300

    adiciona

--- metodos/src/metodo12.java
+++ metodos/src/metodo12.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class metodo12 {
 
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Informe uma palavra : ");
+        String palavra = leitor.next();
+        System.out.println(obterCaracter(palavra));
+    }
+
+    public static char obterCaracter(String palavra) {
+        // return palavra.length() % 2 == 0 ? palavra.charAT(0) : palavra.charAT(1); 
+        if (palavra.length() % 2 == 0) {
+            return palavra.charAt(0);
+        }
+        return palavra.charAt(1);
+    }
 
 }
