f41327534a82fec9cd14c83756cf69be4fb68602
Edoo Scheidt <edooardoscheidt@gmail.com>
2026-08-26 20:54:45 -0300

    .

--- orientacaoAObjeto/veiculo26082026/src/classePrincipal.java
+++ orientacaoAObjeto/veiculo26082026/src/classePrincipal.java
@@ -10,5 +10,15 @@ public class classePrincipal {
         v1.preco = 50.000;
 
         System.out.println(v1.marca + " " + v1.modelo);
+
+
+        veiculo v2 = new veiculo();
+        v2.marca = "Volkswagem";
+        v2.modelo = "Gol";
+        v2.placa = "xx2xx13";
+        v2.ano = 2013;
+        v2.preco = 57.000;
+
+        System.out.printf(v2.marca + " " + v2.modelo);
     }
 }
