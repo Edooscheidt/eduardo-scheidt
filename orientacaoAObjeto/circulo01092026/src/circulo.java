2b2936e40dab9326b09a99980d2d74a80926e3d1
Edoo Scheidt <edooardoscheidt@gmail.com>
2026-09-09 18:59:04 -0300

    .

---
+++ orientacaoAObjeto/circulo01092026/src/circulo.java
@@ -0,0 +1,12 @@
+
+public class circulo {
+
+
+    double raio;
+
+    
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+}
--- orientacaoAObjeto/circulo01092026/src/classePrincipal.java
+++ orientacaoAObjeto/circulo01092026/src/classePrincipal.java
@@ -1,2 +1,22 @@
 public class classePrincipal {
+
+    public static void main(String[] args) {
+
+
+        circulo c1 = new circulo();
+        circulo c2 = new circulo();
+        circulo c3 = new circulo();
+        circulo c4 = new circulo();
+
+
+        c1.raio = 1;
+        c2.raio = 5;
+        c3.raio = 4;
+        c4.raio = 2.5;
+
+
+        System.out.println(c4.calcularArea());
+
+    }
+
 }
