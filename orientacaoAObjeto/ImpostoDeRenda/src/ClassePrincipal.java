507d0bb060376f397c1b48e26b1984c2a6668d69
Edoo Scheidt <edooardoscheidt@gmail.com>
2026-09-16 21:20:59 -0300

    .

--- orientacaoAObjeto/ImpostoDeRenda/src/ClassePrincipal.java
+++ orientacaoAObjeto/ImpostoDeRenda/src/ClassePrincipal.java
@@ -10,6 +10,11 @@ public class ClassePrincipal {
         contribuintes[3] = new ImpostoDeRenda("Ana Costa", "555.666.777-88", "SC", 30000);
         contribuintes[4] = new ImpostoDeRenda("Pedro Santos", "999.888.777-66", "PR", 3500);
 
+        double maiorImposto = 0;
+        String quemPagaMais = "";
+
+        double totalImposto = 0;
+
         for (int i = 0; i < contribuintes.length; i++) {
 
             System.out.println("Nome : " + contribuintes[i].getNome());
@@ -18,6 +23,21 @@ public class ClassePrincipal {
             System.out.printf("Renda anual : R$ %.2f%n", contribuintes[i].getRendaAnual());
             System.out.printf("Imposto a pagar : R$ %.2f%n", contribuintes[i].calcularImposto());
             System.out.println("<----------------------------->");
+
+            if (contribuintes[i].calcularImposto() > maiorImposto) {
+                maiorImposto = contribuintes[i].calcularImposto();
+                quemPagaMais = contribuintes[i].getNome();
+            }
+
+            totalImposto += contribuintes[i].calcularImposto();
         }
+
+        System.out.println();
+        System.out.println("a) Quem mais paga imposto:");
+        System.out.printf("%s - R$ %.2f%n", quemPagaMais, maiorImposto);
+
+        System.out.println();
+        System.out.println("b) Total de imposto pago pelos 5 contribuintes:");
+        System.out.printf("R$ %.2f%n", totalImposto);
     }
 }
